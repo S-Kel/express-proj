905648dd641b161d1afcbbf3759979e2b9ff9e08
Nathan <nathan.fenimore@gmail.com>
2019-01-30 23:02:34 +1000

    modified query to return data as required

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -6,22 +6,13 @@ const dashboardController = (EventWBGS) => {
         try {
 
             // Get all events
-            const events = await EventWBGS.find();
-            // filter out the event_id, organisation, full name, and created at fields
-            let filteredFields = [];
-            events.forEach(event => {
-                const selection = {
-                    event_id: event._id,
-                    organisation: event.host.organisation,
-                    name: `${event.host.first_name} ${event.host.last_name}`,
-                    created_at: event.created_at
-                };
-                filteredFields.push(selection);
-            });
-            // sort on the results on created at, with most recent at the top
-            filteredFields.sort((a, b) => b.created_at - a.created_at);
-            // return the filtered fields as json
-            res.json(filteredFields);
+            const events = await EventWBGS.find()
+                // filter out the event_id, organisation, full name, and created at fields
+                .select('_id host.organisation host.first_name host.last_name created_at')
+                // sort by created_at, with most recent at the top
+                .sort('-created_at');
+            // res.json(filteredFields);
+            res.json(events);
 
         } catch (error) {
             return next(error);
