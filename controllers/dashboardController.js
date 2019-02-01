0b97cadf7978178ed7270c6ef807ce8365c31171
Nathan <nathan.fenimore@gmail.com>
2019-02-01 12:57:58 +1000

    tidied up controller code and added page limiting

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -2,28 +2,49 @@
 
 const dashboardController = (EventWBGS) => {
 
-    const getDashboard = async (req, res, next) => {
+    const getDashboard = (req, res, next) => {
 
         try {
             if (req.params.id) {
                 // get event by id passed in on params
-                const event = await EventWBGS.find({ _id: req.params.id });
+                EventWBGS.findById(req.params.id)
+                    // handle query data
+                    .exec((err, event) => {
+                        // if error, handle error
+                        if (err) return next(err);
 
-                // send response of single event as json
-                res.json(event);
+                        // log success
+                        console.log('Event document retrieved with id: ', event._id);
+
+                        // send response of single event as json
+                        res.json(event);
+                    });
 
             } else {
+
+                const { pageNum, limit } = req.query;
+
                 // get all events
-                const events = await EventWBGS.find()
+                EventWBGS.find()
                     // where denied is not equal to true
                     .where('criteria.denied').ne(true)
                     // filter out the event_id, organisation, full name, and created at fields
                     .select('_id host.organisation host.first_name host.last_name createdAt criteria.shortlisted')
                     // sort by created_at, with most recent at the top
-                    .sort('-createdAt');
+                    .sort('-createdAt')
+                    // pass in page number, and results per page, to limit the number of documents returned
+                    .slice([parseInt((pageNum - 1) * limit), parseInt(limit)])
+                    // handle query data
+                    .exec((err, events) => {
+                        // if error, handle error
+                        if (err) return next(err);
 
-                // send response of all events as json
-                res.json(events);
+                        // log success
+                        console.log(`${events.length} events retrieved from collection`);
+
+                        // send response of all events as json
+                        res.json({ data: events, length: events.length });
+                    });
             };
 
         } catch (error) {
@@ -31,43 +52,47 @@ const dashboardController = (EventWBGS) => {
         };
     };
 
-    const updateDashboard = async (req, res, next) => {
+    const updateDashboard = (req, res, next) => {
         // updates criteria model embedded in event model with data from the request body. These data include:
         // socials_check, description_check, volunteers_check, target_value_check, location_check, best_date_check, 
         // key_influencers_check, shortlisted, denied, denied_reason,
         try {
 
-            // get id of event to update from params
-            const id = req.params.id;
-
             // update event criteria with values sent on request body
-            const updatedEvent = await EventWBGS.findOneAndUpdate({ _id: id }, {
-                criteria: req.body
-            }, (err, query) => {
-                if (err) next(err);
-                console.log('Criteria updated for Event document id: ', query._id);
-            });
+            EventWBGS.findByIdAndUpdate(req.params.id, { criteria: req.body }, { new: true })
+                // handle query data
+                .exec((err, updatedEvent) => {
+                    // if error, handle error
+                    if (err) return next(err);
 
-            // if event has been denied, pass to denied email middleware with email and first name of host
-            if (updatedEvent.criteria.denied) {
-                req.body.email = updatedEvent.host.user.email;
-                req.body.first_name = updatedEvent.host.first_name;
-                return next();
-            };
+                    // log success
+                    console.log('Criteria updated for Event document id: ', updatedEvent._id);
 
-            // otherwise, respond with ok
-            res.status(200).send('content updated');
+                    // if event has been denied, pass to denied email middleware with email and first name of host
+                    if (updatedEvent.criteria.denied) {
+                        req.body.email = updatedEvent.host.user.email;
+                        req.body.first_name = updatedEvent.host.first_name;
+                        return next();
+                    };
+
+                    // otherwise, respond with ok
+                    res.status(200).send('content updated');
+                });
+
         }
         catch (error) {
             return next(error);
         };
     };
 
-    const getShortlist = async (req, res, next) => {
+    const getShortlist = (req, res, next) => {
 
         try {
-            // get all events 
-            const shortlist = await EventWBGS.find()
+
+            const { pageNum, limit } = req.query;
+
+            // get all events on the shortlist
+            EventWBGS.find()
                 // where denied is not equal to true
                 .where('criteria.denied').ne(true)
                 // where shortlist is true
@@ -75,10 +100,21 @@ const dashboardController = (EventWBGS) => {
                 // filter out the event_id, organisation, full name, and created at fields
                 .select('_id host.organisation host.first_name host.last_name createdAt criteria.shortlisted')
                 // sort by created_at, with most recent at the top
-                .sort('-createdAt');
+                .sort('-createdAt')
+                // pass in page number, and results per page, to limit the number of documents returned
+                .slice([parseInt((pageNum - 1) * limit), parseInt(limit)])
+                // handle query data
+                .exec((err, shortlist) => {
+                    // if error, handle error
+                    if (err) return next(err);
 
-            // send response of all events as json
-            res.json(shortlist);
+                    // log success
+                    console.log(`${shortlist.length} events retrieved from collection for shortlist`);
+
+                    // send response of all events as json
+                    res.json({ data: shortlist, length: shortlist.length });
+                });
+
         }
         catch (error) {
             return next(error);
