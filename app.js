c787bbcd5b4c552dda6ead099d31f20e5c5b4eb2
Nathan <nathan.fenimore@gmail.com>
2019-02-04 09:39:08 +1000

    added docs folder and README

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const session = require('express-session');
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
